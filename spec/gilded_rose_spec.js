43b0b55061473af63512654b937278947663b954
if23b266 <if23b266@technikum-wien.at>
2024-06-02 18:25:52 +0200

    Test: increase quality of Backstage passes by 2 when 10 days or less

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -34,4 +34,10 @@ describe("Gilded Rose", function() {
     expect(items[0].sell_in).toEqual(14);
     expect(items[0].quality).toEqual(21);
   });
+  it("should increase quality of Backstage passes by 2 when 10 days or less", function() {
+    items = [ new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20) ];
+    update_quality();
+    expect(items[0].sell_in).toEqual(9);
+    expect(items[0].quality).toEqual(22);
+  });
 });
