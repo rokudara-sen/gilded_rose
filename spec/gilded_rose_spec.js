c4158877e2dce5a99e669a781d4256b21ad7b628
if23b266 <if23b266@technikum-wien.at>
2024-06-02 18:22:51 +0200

    Test: decrease quality and sell_in of normal items

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,9 +1,10 @@
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
+  it("should decrease quality and sell_in of normal items", function() {
+    items = [ new Item("Elixir of the Mongoose", 5, 7) ];
     update_quality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].sell_in).toEqual(4);
+    expect(items[0].quality).toEqual(6);
   });
-
+  
 });
