a06f57d8f446af20d453eaebc03467337415c5c7
if23b266 <if23b266@technikum-wien.at>
2024-06-02 18:30:29 +0200

    Implement handling of Conjured items

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -19,6 +19,9 @@ function update_quality() {
       if (items[i].quality > 0) {
         if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
           items[i].quality = items[i].quality - 1
+          if (items[i].name.startsWith('Conjured')) {
+            items[i].quality = items[i].quality - 1;
+          }
         }
       }
     } else {
@@ -47,6 +50,9 @@ function update_quality() {
           if (items[i].quality > 0) {
             if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
               items[i].quality = items[i].quality - 1
+              if (items[i].name.startsWith('Conjured')) {
+                items[i].quality = items[i].quality - 1;
+              }
             }
           }
         } else {
